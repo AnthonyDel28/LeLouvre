bf6ecbc1d3f88ce9333a19eae33dd2a894cd373f
AnthonyDel28 <anthonydelmeire2709@gmail.com>
2022-11-01 11:55:52 +0100

    update 01_11_2022

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PaintingRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class GalleryController extends AbstractController
 {
     #[Route('/gallery', name: 'gallery')]
-    public function gallery(PaintingRepository $paintingRepository): Response
+    public function gallery(PaintingRepository $paintingRepository, CategoryRepository $categoryRepository): Response
     {
         $paintings = $paintingRepository->findAll();
+        $categories = $categoryRepository->findAll();
         return $this->render('pages/gallery.html.twig',
             [
                 'paintings' => $paintings,
+                'categories' => $categories
             ]
         );
     }
